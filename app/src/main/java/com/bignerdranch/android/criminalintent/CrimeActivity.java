24d3d75f6273c5605498003f8e27446c6d081784
a-saeed <31377230+a-saeed@users.noreply.github.com>
2019-07-05 19:35:05 +0200

    renaming activity_main.xml to activity_fragment.xml hence updating CrimeActivity

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeActivity.java
@@ -11,7 +11,7 @@ public class CrimeActivity extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_fragment);
 
         FragmentManager fm = getSupportFragmentManager();
         /*retrieve an existing fragment by its
