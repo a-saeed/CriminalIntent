e889a65973355add1c62b6c2e7668b603ee47c41
a-saeed <31377230+a-saeed@users.noreply.github.com>
2019-07-06 19:17:48 +0200

    adding an adapter to CrimeListFragment's ListView

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimeListFragment.java
@@ -1,6 +1,8 @@
 package com.bignerdranch.android.criminalintent;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
 
 import androidx.fragment.app.ListFragment;
 
@@ -16,5 +18,18 @@ public class CrimeListFragment extends ListFragment {
         getActivity().setTitle(R.string.crimes_title);
         //access list of crimes stored in crimeLab
         mCrimes = CrimeLab.get(getActivity()).getCrimes();
+
+        //create an instance of ArrayAdapter<T>
+        // and make it the adapter for CrimeListFragment’s ListView
+        ArrayAdapter<Crime> adapter =
+                new ArrayAdapter<Crime>(getActivity(),
+                        android.R.layout.simple_expandable_list_item_1,
+                        mCrimes);
+        //setListAdapter(ListAdapter) method is a
+        // ListFragment convenience method that you can use to
+        // set the adapter of the implicit ListView managed by CrimeListFragment.
+        setListAdapter(adapter);
     }
+
+
 }
