18e6565ac8ba3a7c231e5f6806e26dd3898cecc2
a-saeed <31377230+a-saeed@users.noreply.github.com>
2019-07-10 09:02:53 +0200

    creating a viewpager class to replace CrimeActivity

---
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimePagerActivity.java
@@ -0,0 +1,13 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.FragmentActivity;
+
+public class CrimePagerActivity extends FragmentActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+}
