f5ff21a4d7cbaff32de427b73f74e501c1a19869
a-saeed <31377230+a-saeed@users.noreply.github.com>
2019-07-10 09:41:48 +0200

    creating content view programmatically and setting up pager adapter

--- app/src/main/java/com/bignerdranch/android/criminalintent/CrimePagerActivity.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent/CrimePagerActivity.java
@@ -2,12 +2,44 @@ package com.bignerdranch.android.criminalintent;
 
 import android.os.Bundle;
 
+import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import java.util.ArrayList;
 
 public class CrimePagerActivity extends FragmentActivity {
+    private ViewPager mViewPager;
+    private ArrayList<Crime> mCrimes;
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+        //instantiate viewPager and set it as the content view
+        //(instead of creating new XMl layout file , we create this simple layout in code.)
+        mViewPager = new ViewPager(this);
+        mViewPager.setId(R.id.viewPager);
+        setContentView(mViewPager);
+
+        mCrimes = CrimeLab.get(this).getCrimes();
+
+        //setting up the adapter
+        FragmentManager fm = getSupportFragmentManager();
+        mViewPager.setAdapter(new FragmentStatePagerAdapter(fm) {
+            @Override
+            public Fragment getItem(int position) {
+                //return a CrimeFragment configured
+                //to display the crime at that position
+                Crime crime = mCrimes.get(position);
+                return CrimeFragment.newInstance(crime.getmId()) ;
+            }
+
+            @Override
+            public int getCount() {
+                return mCrimes.size();
+            }
+        });
     }
 }
